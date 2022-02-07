b53949275a7bc2e1fb77deaaed60b7c3f64f3df0
soma_asahi <95704277+somaasahi@users.noreply.github.com>
2022-02-08 00:00:22 +0900

    a

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 require('./bootstrap');
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/brands";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
