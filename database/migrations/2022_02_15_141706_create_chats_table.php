16ecc32485c9e2e1bd483dd0da1c608557c67f0a
soma_asahi <95704277+somaasahi@users.noreply.github.com>
2022-02-15 23:41:18 +0900

    create chatstable

--- database/migrations/2022_02_15_141706_create_chats_table.php
+++ database/migrations/2022_02_15_141706_create_chats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProfileTable extends Migration
+class CreateChatsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateProfileTable extends Migration
      */
     public function up()
     {
-        Schema::create('profile', function (Blueprint $table) {
+        Schema::create('chats', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id')->unsigned();
-            $table->integer('age');
-            $table->integer('income');
-            $table->longText('introduce');
+            $table->text('chat');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
@@ -31,6 +29,7 @@ class CreateProfileTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profile');
+        Schema::dropIfExists('chats');
     }
 }
+
